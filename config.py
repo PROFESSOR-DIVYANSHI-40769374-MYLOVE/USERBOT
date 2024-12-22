259d3e66d79c10a0fbf8c6adcf193046df86631a
PROFESSOR-DIVYANSHI-40769374-MYLOVE <166273111+PROFESSOR-DIVYANSHI-40769374-MYLOVE@users.noreply.github.com>
2024-12-22 14:53:58 +0530

    [ 𝗣𝗥𝚯𝗙𝗘𝗦𝗦𝚯𝗥 ][ ѕαиαтαиι ]

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "25504446")) #optional
+API_HASH = getenv("API_HASH", "47db27cde56c3e4690e244e6de10f919") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
